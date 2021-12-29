6c425236d15d70392d91e146d1575d265529c952
matanv <matanv@gmail.com>
2021-12-29 21:10:06 +0200

    WIP

--- src/components/Draw.vue
+++ src/components/Draw.vue
@@ -1,3 +1,13 @@
 <template>
-  <div>Draw</div>
+  <div>
+    <canvas v-ref="canvasEl"></canvas>
+  </div>
 </template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+const canvasEl = ref("");
+onMounted(() => {
+  console.log(canvasEl.value);
+});
+</script>
